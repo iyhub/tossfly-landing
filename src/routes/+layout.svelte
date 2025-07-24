17bc74aed65274ce15c9e541b95e3050a0e57b11
zy <zhuyongdev@163.com>
2025-07-25 00:07:16 +0800

    clarity tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
 	import '../app.css';
 	import Footer from '$lib/components/sections/Footer.svelte';
+	import { onMount } from 'svelte';
+	import { PUBLIC_CLARITY_ID } from '$env/static/public';
 
 	let { children } = $props();
+
+	onMount(() => {
+		const script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.innerHTML = `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", '${PUBLIC_CLARITY_ID}');
+  `;
+		document.head.appendChild(script);
+	});
 </script>
 
 <main class="mx-auto max-w-screen-2xl p-2">
